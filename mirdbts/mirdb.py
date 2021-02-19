e0d506f553b75f9ee94dd18b351bc17553ad4e13
alex123012 <makhonin.a.ru@gmail.com>
2021-02-19 17:14:45 +0300

    refactor mirdb

--- mirdbts/mirdb.py
+++ mirdbts/mirdb.py
@@ -8,17 +8,21 @@ class CustomMiRDB:
     url = 'http://mirdb.org/cgi-bin/custom.cgi'
 
     def __init__(self, sequence=None, type_='miRNA', species='hsa'):
+
         if 'mirna' in type_.lower():
             self.type_ = 'miRNA'
             self.sequence = sequence.upper()
+
         elif 'mrna' in type_.lower():
             self.type_ = 'mRNATarget'
             self.sequence = sequence.upper() + 'a'
+
         self.species = species.lower()
 
         self.__startsession()
 
     def __filefind(self, temp):
+
         soup = bs(temp.text, "html.parser")
         table = soup.findAll("input")
         return table[0]['value']
@@ -26,19 +30,21 @@ class CustomMiRDB:
     def __startsession(self):
 
         self.session = requests.Session()
-        dictir = {'searchSpecies': self.species,
-                  'subChoice': self.type_,
-                  'customSub': self.sequence,
-                  '.submit': 'Go'}
-        temp = self.session.post(self.url, data=dictir)
+
+        data = {'searchSpecies': self.species,
+                'subChoice': self.type_,
+                'customSub': self.sequence,
+                '.submit': 'Go'}
+        temp = self.session.post(self.url, data=data)
 
         self.filename = self.__filefind(temp)
 
-        dictir = {'.submit': 'Retrieve Prediction Result',
-                  'fileName': self.filename}
-        self.responce = self.session.post(self.url, data=dictir)
-        self.session.post(self.url,
-                          data={".submit": "Return to Custom Prediction"})
+        data = {'.submit': 'Retrieve Prediction Result',
+                'fileName': self.filename}
+        self.responce = self.session.post(self.url, data=data)
+
+        data = {".submit": "Return to Custom Prediction"}
+        self.session.post(self.url, data=data)
 
     def __repr__(self):
         return self.responce.text
@@ -48,31 +54,28 @@ class CustomMiRDB:
 
 
 class Mirdb(pd.DataFrame):
-    def __init__(self, html, dictir=None):
-        __rows, __headers = self.__get_table_rows(html, dictir)
+    def __init__(self, html, html_dict=None):
+
+        __rows, __headers = self.__get_table_rows(html, html_dict)
+
         super().__init__(__rows, columns=__headers)
         try:
             self.set_index('Target Rank', inplace=True)
             self.drop(['Target Detail', 'miRNA Name', 'Gene Description'],
                       axis=1, inplace=True)
         except KeyError:
-            print('lol')
+            pass
 
-    def __get_table_rows(self, html, dictir):
+    def __get_table_rows(self, html, html_dict):
 
         """Given a url, returns all its table rows and headers"""
 
-        if not dictir:
-            dictir = {'border': "1", 'id': "table1",
-                      'style': "border-collapse: collapse"}
-
-        # использовать, если по url, а не по html
-#         reques = requests.get(url)
-#         print(reques)
-#         html = url.text
+        if not html_dict:
+            html_dict = {'border': "1", 'id': "table1",
+                         'style': "border-collapse: collapse"}
 
         soup = bs(str(html), "html.parser")
-        table = soup.findAll("table", dictir)[0]
+        table = soup.findAll("table", html_dict)[0]
 
         rows = list(map(self.__get_row, table.find_all("tr")))
 
@@ -85,10 +88,11 @@ class Mirdb(pd.DataFrame):
     def __get_cell(self, td):
 
         res = td.text.strip()
-        # x = td.select_one('a')
 
         # Если в датафрем нужны ссылки на детали
         # по каждой микроРНК - можно раскомментить
+
+        # x = td.select_one('a')
         # if x and x['href'].startswith('/'):
         #     return f'<a target="_blank" href="http://mirdb.org{x['href']}">{res}</a>'
         # elif x and x['href'].startswith('http'):
